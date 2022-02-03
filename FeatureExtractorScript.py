adfcf52d57112d39115e9e8975528a44c89a6cb2
Aamir <djzaamir@pop-os.localdomain>
2022-02-03 13:17:03 +0500

    Seperated the LLVM parser code into a seperate dir, Currently working on python script that will scan a Kernels dir, and for each kernel found in that dir, it will pass it through the Parser for feature extraction

---
+++ FeatureExtractorScript.py
@@ -0,0 +1,21 @@
+import os
+
+
+
+
+def main():
+    
+    # This path points to the compiled Shared Library Object which is build earlier
+    # This shared library object contains the LLVM pass code to analyze and extract the features
+    # From the Kernel.cl file, which will be specified by this python script
+    parser_lib_path = "./Parser/build"
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
